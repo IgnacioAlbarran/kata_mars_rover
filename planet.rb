22a5e0501e18cc4ca2dca5c6a69ffaf35e459e45
Ignacio Albarrán <ignacioalbarran@emergia.net>
2020-09-05 21:47:03 +0200

    move map checking and coordinate and obstacle calculation to planet

--- planet.rb
+++ planet.rb
@@ -8,4 +8,20 @@ class Planet
     }
     @obstacles = [[1, 5], [2, 3], [3, 4]]
   end
+
+  def next_coordinates(point, dir, command)
+    return [point.x, point.y + 1] if (dir == 'N' && command == 'f') || (dir == 'S' && command == 'b')
+    return [point.x, point.y - 1] if (dir == 'S' && command == 'f') || (dir == 'N' && command == 'b')
+    return [point.x + 1, point.y] if (dir == 'E' && command == 'f') || (dir == 'W' && command == 'b')
+    return [point.x - 1, point.y] if (dir == 'W' && command == 'f') || (dir == 'E' && command == 'b')
+  end
+
+  def check_map(point)
+    point.x = planet[:x].to_a[(point.x - 1) % 5]
+    point.y = planet[:y].to_a[(point.y - 1) % 5]
+  end
+
+  def obstacle?(coordinates)
+    obstacles.include?(coordinates)
+  end
 end
--- rover.rb
+++ rover.rb
@@ -18,13 +18,7 @@ class Rover
     @dir
   end
 
-  def get_direction
-    @dir
-  end
-
   def commands(commands_array)
-    save_last_point
-
     commands_array.each do |command|
       move(command) if (command == 'f' || command == 'b')
       turn(command) if (command == 'r' || command == 'l')
@@ -34,15 +28,10 @@ class Rover
   private
 
   def move(command)
-    next_coordinates = nil
-    next_coordinates = @point.x, @point.y + 1 if (@dir == 'N' && command == 'f') || (@dir == 'S' && command == 'b')
-    next_coordinates = @point.x, @point.y - 1 if (@dir == 'S' && command == 'f') || (@dir == 'N' && command == 'b')
-    next_coordinates = @point.x + 1, @point.y if (@dir == 'E' && command == 'f') || (@dir == 'W' && command == 'b')
-    next_coordinates = @point.x - 1, @point.y if (@dir == 'W' && command == 'f') || (@dir == 'E' && command == 'b')
-
-    obstacle?(next_coordinates) && 'STOPPED: There is an obstacle!!' && return
-    @point = Point.new(next_coordinates[0],next_coordinates[1])
-    check_map
+    next_coordinates = @mars.next_coordinates(@point, @dir, command)
+    return if @mars.obstacle?(next_coordinates)
+    @point = Point.new(next_coordinates[0], next_coordinates[1])
+    @mars.check_map(@point)
   end
 
   def turn(orientation)
@@ -51,22 +40,5 @@ class Rover
     else
       @dir = DIRECTIONS[(DIRECTIONS.index{ |i| i == dir } - 1)]
     end
-  end
-
-  def check_map
-    @point.x = @mars.planet[:x].to_a[(@point.x - 1) % 5]
-    @point.y = @mars.planet[:y].to_a[(@point.y - 1) % 5]
-  end
-
-  def obstacle?(coordinates)
-    @mars.obstacles.include?(coordinates)
-  end
-
-  def save_last_point
-    @last_point = give_position
-  end
-
-  def x
-    self.x
   end
 end
