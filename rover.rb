f2d038937cb90bd6a357761f66d414fd47f30eb3
Ignacio Albarrán <ignacioalbarran@emergia.net>
2020-09-05 19:00:29 +0200

    refactor obstacle detection and move method

--- rover.rb
+++ rover.rb
@@ -34,11 +34,14 @@ class Rover
   private
 
   def move(command)
-    @point = Point.new(@point.x, @point.y + 1) if (@dir == 'N' && command == 'f') || (@dir == 'S' && command == 'b')
-    @point = Point.new(@point.x, @point.y - 1) if (@dir == 'S' && command == 'f') || (@dir == 'N' && command == 'b')
-    @point = Point.new(@point.x + 1, @point.y) if (@dir == 'E' && command == 'f') || (@dir == 'W' && command == 'b')
-    @point = Point.new(@point.x - 1, @point.y) if (@dir == 'W' && command == 'f') || (@dir == 'E' && command == 'b')
+    next_coordinates = nil
+    next_coordinates = @point.x, @point.y + 1 if (@dir == 'N' && command == 'f') || (@dir == 'S' && command == 'b')
+    next_coordinates = @point.x, @point.y - 1 if (@dir == 'S' && command == 'f') || (@dir == 'N' && command == 'b')
+    next_coordinates = @point.x + 1, @point.y if (@dir == 'E' && command == 'f') || (@dir == 'W' && command == 'b')
+    next_coordinates = @point.x - 1, @point.y if (@dir == 'W' && command == 'f') || (@dir == 'E' && command == 'b')
 
+    obstacle?(next_coordinates) && 'STOPPED: There is an obstacle!!' && return
+    @point = Point.new(next_coordinates[0],next_coordinates[1])
     check_map
   end
 
@@ -53,11 +56,6 @@ class Rover
   def check_map
     @point.x = @mars.planet[:x].to_a[(@point.x - 1) % 5]
     @point.y = @mars.planet[:y].to_a[(@point.y - 1) % 5]
-    stop_move if obstacle?(give_position)
-  end
-
-  def stop_move
-    @point.x, @point.y = @last_point[0], @last_point[1]
   end
 
   def obstacle?(coordinates)
@@ -66,5 +64,9 @@ class Rover
 
   def save_last_point
     @last_point = give_position
+  end
+
+  def x
+    self.x
   end
 end
